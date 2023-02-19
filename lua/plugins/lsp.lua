a5e687f580e125f3d101eaee48cad4f3fe336b79
antoniomayk <antoniomayk.g@gmail.com>
2023-02-19 11:45:44 -0300

    feat: add support for common key mappings in lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,21 @@
 local lspconfig = require("lspconfig")
 
+local lsp_keys = function(bufopts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+end
+
 local on_attach = function(_, bufnr)
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    local opts = { noremap = true, silend = true }
+    local bufopts = { table.unpack(opts), buffer = bufnr }
+
+    lsp_keys(bufopts)
 
     local which_key = require "which-key"
 
